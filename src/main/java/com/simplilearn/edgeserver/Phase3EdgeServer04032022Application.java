d52dc4df6b99e0e4be2e23e72bfaba4cb7d1053a
Wahid Khan <wahid.khan74@gmail.com>
2022-04-03 14:10:11 +0000

    added zuul proxy server dependencies

---
+++ src/main/java/com/simplilearn/edgeserver/Phase3EdgeServer04032022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3EdgeServer04032022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3EdgeServer04032022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/edgeserver/Phase3EdgeServer04032022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3EdgeServer04032022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
