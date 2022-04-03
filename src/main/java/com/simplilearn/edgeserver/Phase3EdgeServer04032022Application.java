0064f5d9f3d241ef92ca80407584c852ab33687d
Wahid Khan <wahid.khan74@gmail.com>
2022-04-03 14:30:45 +0000

    added proxy edge server

--- src/main/java/com/simplilearn/edgeserver/Phase3EdgeServer04032022Application.java
+++ src/main/java/com/simplilearn/edgeserver/Phase3EdgeServer04032022Application.java
@@ -2,8 +2,10 @@ package com.simplilearn.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class Phase3EdgeServer04032022Application {
 
 	public static void main(String[] args) {
